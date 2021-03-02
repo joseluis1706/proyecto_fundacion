10d523a7bc3be3fdff7f332765b3ae29af18f46d
Claudia Belforte <claubelforte@gmail.com>
2021-03-02 02:20:50 -0300

    falta el footer version pc

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ window.addEventListener("scroll", function(){
     var nav = document.querySelector("nav")
     nav.classList.toggle("sticky", window.scrollY > 0);
 })
+
+
+ScrollReveal().reveal('.eventos' , { delay: 900 });
+ScrollReveal().reveal('.card ' , { delay: 800 });
+ScrollReveal().reveal('p ' , { delay: 900 });
+ScrollReveal().reveal('h5 ', { delay: 900 });
+ScrollReveal().reveal('.btn ' , { delay: 900 });
+ScrollReveal().reveal('footer ' , { delay: 900 });
+ScrollReveal().reveal('iframe ' , { delay: 900 });
+ScrollReveal().reveal('.logo-footer' , { delay: 1400 });
+ScrollReveal().reveal('li' , { delay: 1400 });
