49a5301722a2de6958beaf23ead196690c185a77
Priscilla Rojas <priscilla.k.rojas@hotmail.com>
2021-03-06 09:49:53 -0300

    realice modificaciones, en el slider solo para el funcionamiento

--- slider.js
+++ slider.js
@@ -8,14 +8,14 @@ const btnRight = document.querySelector("#btn-right");
 slider.insertAdjacentElement('afterbegin', silderSectionLast);
 
 function moverDerecha() {
-    let silderSectionFirst = document.querySelectorAll(".slider__Section")[0];
-    slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 0.5s";
+    let sliderSectionFirst = document.querySelectorAll('.slider__section')[0];
+    slider.style.marginLeft = '-200%';
+    slider.style.transition = 'all 0.5s';
     setTimeout(function(){
-        slider.style.transition = "none";
-        slider.insertAdjacentElement('beforeend', silderSectionFirst);
-        slider.style.marginLeft = "-100%";
-    }, 500);
+        slider.style.transition = 'none';
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = '-100%';
+    }, 500)
 }
 
 
@@ -40,5 +40,5 @@ btnLeft.addEventListener('click', function(){
 });
 
 setInterval(function(){
-    moverIzquierda();
-}, 5000);
+    moverDerecha();
+}, 4000);
