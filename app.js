32e3f713d12866939d16b35e0a767e9baeab2734
Priscilla Rojas <priscilla.k.rojas@hotmail.com>
2021-03-19 10:48:15 -0300

    inicie con el javascript

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ window.addEventListener("scroll", function(){
     nav.classList.toggle("sticky", window.scrollY > 0);
 })
 
+let cerrar = document.getElementsByClassName('.close');
+let abrir = document.getElementById('.cta');
+let modal = document.getElementsByClassName('.modal');
+let modalContainer = document.getElementsByClassName('.modal-container');
 
-ScrollReveal().reveal('.eventos' , { delay: 700 });
-ScrollReveal().reveal('.card ' , { delay: 700 });
-ScrollReveal().reveal('p ' , { delay: 700 });
-ScrollReveal().reveal('h5 ', { delay: 700 });
-ScrollReveal().reveal('.btn ' , { delay: 700 });
-ScrollReveal().reveal('footer ' , { delay: 600 });
-ScrollReveal().reveal('iframe ' , { delay: 600 });
-ScrollReveal().reveal('.logo-footer' , { delay: 1000 });
-ScrollReveal().reveal('li' , { delay: 1000 });
+abrir.addEventListener('click', function(){
+    modalContainer.style.opacity = '1';
+    modalContainer.style.visibility = 'visible';
+})
