4b170cce6a6227f2dbe992e2475d557f3e8773f2
Priscilla Rojas <priscilla.k.rojas@hotmail.com>
2021-03-19 14:40:23 -0300

    realise la parteb de javascript del modal

--- app.js
+++ app.js
@@ -3,12 +3,32 @@ window.addEventListener("scroll", function(){
     nav.classList.toggle("sticky", window.scrollY > 0);
 })
 
-let cerrar = document.getElementsByClassName('.close');
-let abrir = document.getElementById('.cta');
-let modal = document.getElementsByClassName('.modal');
-let modalContainer = document.getElementsByClassName('.modal-container');
+var abrir = document.getElementById('cta');
+var cerrar = document.getElementById('close');
+var modalContainer = document.getElementById('modal-container');
+var modal = document.getElementById('modal-apadrinar');
 
-abrir.addEventListener('click', function(){
-    modalContainer.style.opacity = '1';
+abrir.addEventListener("click", function(){
     modalContainer.style.visibility = 'visible';
+    modalContainer.style.opacity = '1';
+    modal.classList.toggle('modal-close');
+});
+
+cerrar.addEventListener('click', function(){
+    modal.classList.toggle('modal-close');
+    
+    setTimeout(() => {
+        modalContainer.style.visibility = 'hidden';
+        modalContainer.style.opacity = '0';
+        },500);
+});
+window.addEventListener('click', function(e){
+    
+    if(e.target == modalContainer){
+        modal.classList.toggle('modal-close');
+        setTimeout(() => {
+            modalContainer.style.visibility = 'hidden';
+            modalContainer.style.opacity = '0';
+        },500);
+    }
 })
